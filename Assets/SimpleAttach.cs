2c657b37c1bc2aaa40e3cd9146ba82bc15c366f6
Carter <cartersheffield12@gmail.com>
2020-01-31 16:31:57 -0600

    Gun in scene
    added gun and gun scripting

---
+++ Assets/SimpleAttach.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+
+public class SimpleAttach : MonoBehaviour
+{
+
+    private Interactable interactable;
+    // Start is called before the first frame update
+    void Start()
+    {
+        interactable = GetComponent<Interactable>();
+    }
+
+    // Update is called once per frame
+    private void OnHandHoverBegin(Hand hand)
+    {
+        hand.ShowGrabHint();
+    }
+    private void OnHandHoverEnd(Hand hand)
+    {
+        hand.HideGrabHint();
+    }
+    private void OnHandHoverUpdate(Hand hand)
+    {
+        GrabTypes grabType = hand.GetGrabStarting();
+        bool isGrabEnding = hand.IsGrabEnding(gameObject);
+
+        //GRAB THE OBJECT
+        if(interactable.attachedToHand == null && grabType != GrabTypes.None)
+        {
+            hand.AttachObject(gameObject, grabType);
+        }
+        //RELEASE
+        else if (isGrabEnding)
+        {
+            hand.DetachObject(gameObject);
+        }
+    }
+}
