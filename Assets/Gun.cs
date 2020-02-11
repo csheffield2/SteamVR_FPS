104b06cf8a70dc48818150f737f09d9f1a9ce6c6
serigne007mor <serignemortoure1@gmail.com>
2020-02-10 23:18:35 -0600

    got the bullets to shoot

--- Assets/Gun.cs
+++ Assets/Gun.cs
@@ -27,6 +27,10 @@ public class Gun : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Fire();
+        }
         if(interactable.attachedToHand != null)
         {
             SteamVR_Input_Sources source = interactable.attachedToHand.handType;
@@ -41,6 +45,7 @@ public class Gun : MonoBehaviour
     void Fire()
     {
         Debug.Log("Fired");
+        // bullet<MeshRenderer>.enabled = true;
         Rigidbody bulletrb = Instantiate(bullet, barrelPivot.position, barrelPivot.rotation).GetComponent<Rigidbody>();
         bulletrb.velocity = barrelPivot.forward * shootingSpeed;
         muzzleFlash.SetActive(true);
