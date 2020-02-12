1104de1211fefce4ecaf2bb463536ce51eb5b089
serigne007mor <serignemortoure1@gmail.com>
2020-02-12 02:12:54 -0600

    added ballons and made bullet pop them, need to add counter

--- Assets/bulletScript.cs
+++ Assets/bulletScript.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class bulletScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnCollisionEnter(Collision collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        collision.gameObject.SendMessageUpwards("ApplyDamage",SendMessageOptions.DontRequireReceiver);
     }
 }
